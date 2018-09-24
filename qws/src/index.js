ee241b101ec477d1649a3b17e3abf713495d720a
Chris <christonechen@gmail.com>
2018-09-24 14:59:47 -0700

    nav bar

--- qws/src/index.js
+++ qws/src/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './nav.js';
+import './index.css';
 
 class MainPage extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {page : 1};
+		this.handlePageChange = this.handlePageChange.bind(this);
+	}
+	handlePageChange(event){
+		this.setState({page : event.target.id}); 
+		console.log("page", event.target.id);
 	}
 	render(){
-		return(
-			<div classname="title">
-				<h3>
-					TITLE
-				</h3>
-			</div>
-		
+		let page = this.state.page;
+		return(<div>
+			<h3>
+				TITLE
+			</h3>
+
+			<NavBar handlePageChange={this.handlePageChange}></NavBar>
+			
+			<p> words {page}</p>
+			
+		</div>
 
 		);
 	}
---
+++ qws/src/nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './nav.css';
+
+class NavBar extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.handlePageChange = this.props.handlePageChange.bind(this);
+	}
+	handleClick(event){
+		event.preventDefault();
+		console.log(event.target.id);
+		this.handlePageChange(event);
+	}
+	render(){
+		return(<div>
+			<ul>
+				<li id='1' onClick={this.handleClick}>1</li>
+				<li id='2' onClick={this.handleClick}>2</li>
+				<li id='3' onClick={this.handleClick}>3</li>
+			</ul>
+			</div>
+
+		);
+	}
+	
+}
+export default NavBar;
